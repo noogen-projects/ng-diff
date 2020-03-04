2fa0c7e179d5843e6b53752ce0e9e48493142aaf
Alexander XX <freecoder.xx@gmail.com>
2020-03-04 13:09:03 +0300

    Use IntoIterator for seqs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! O(m + n) space.
 //! http://www.mathcs.emory.edu/~cheung/Courses/323/Syllabus/DynProg/Docs/Hirschberg=Linear-space-LCS.pdf
 
-use std::ops::{Index, IndexMut};
+use std::ops::IndexMut;
 
 /// The interface of the Needleman-Wunsch score matrix line
 pub trait NwScoreLine: IndexMut<usize, Output = usize> {
@@ -16,41 +16,28 @@ impl NwScoreLine for Vec<usize> {
     }
 }
 
-pub trait SequenceCollection: Index<usize> {
-    fn len(&self) -> usize;
-}
-
-impl<T> SequenceCollection for [T] {
-    fn len(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<T> SequenceCollection for Vec<T> {
-    fn len(&self) -> usize {
-        self.len()
-    }
-}
-
 /// This function returns the last line of the Needleman-Wunsch score matrix
-pub fn score_last_line<SeqA, SeqB, Line>(a: &SeqA, b: &SeqB) -> Line
+pub fn score_last_line<SeqA, SeqB, Line>(a: SeqA, b: SeqB) -> Line
 where
-    SeqA: SequenceCollection + ?Sized,
-    SeqB: SequenceCollection + ?Sized,
-    SeqA::Output: PartialEq<SeqB::Output>,
+    SeqA: IntoIterator,
+    SeqB: IntoIterator,
+    SeqA::IntoIter: ExactSizeIterator,
+    SeqB::IntoIter: ExactSizeIterator + Clone,
+    SeqA::Item: PartialEq<SeqB::Item>,
     Line: NwScoreLine,
 {
+    let (a, b) = (a.into_iter(), b.into_iter());
     let mut penult = Line::new(b.len());
     let mut last = Line::new(b.len());
 
-    for i in 0..a.len() {
+    for a in a {
         let tmp = penult;
         penult = last;
         last = tmp;
         let mut prev_penult = 0;
         let mut prev_last = 0;
-        for j in 0..b.len() {
-            if a[i] == b[j] {
+        for (j, b) in b.clone().enumerate() {
+            if a == b {
                 last[j] = prev_penult + 1;
             } else {
                 last[j] = prev_last.max(penult[j]);
@@ -62,36 +49,28 @@ where
     last
 }
 
-pub fn score_last_line_for_slices<T, Line>(a: &[T], b: &[T]) -> Line
-where
-    T: PartialEq,
-    Line: NwScoreLine,
-{
-    score_last_line(a, b)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_score_last_line() {
-        let last: Vec<_> = score_last_line_for_slices(b"", b"");
+        let last: Vec<_> = score_last_line(b"", b"");
         assert_eq!(last, vec![]);
 
-        let last: Vec<_> = score_last_line_for_slices(b"AGTACGCA", b"");
+        let last: Vec<_> = score_last_line(b"AGTACGCA", b"");
         assert_eq!(last, vec![]);
 
-        let last: Vec<_> = score_last_line_for_slices(b"", b"TATGC");
+        let last: Vec<_> = score_last_line(b"", b"TATGC");
         assert_eq!(last, vec![0, 0, 0, 0, 0]);
 
-        let last: Vec<_> = score_last_line_for_slices(b"AGTACGCA", b"TATGC");
+        let last: Vec<_> = score_last_line(b"AGTACGCA", b"TATGC");
         assert_eq!(last, vec![1, 2, 2, 3, 4]);
 
-        let last: Vec<_> = score_last_line_for_slices(b"ABCBDAB", b"BDCABA");
+        let last: Vec<_> = score_last_line(b"ABCBDAB", b"BDCABA");
         assert_eq!(last, vec![1, 2, 2, 3, 4, 4]);
 
-        let last: Vec<_> = score_last_line_for_slices(b"BDCABA", b"ABCBDAB");
+        let last: Vec<_> = score_last_line(b"BDCABA", b"ABCBDAB");
         assert_eq!(last, vec![1, 2, 2, 3, 3, 4, 4]);
 
         let last: Vec<_> = score_last_line(&Vec::from("BDCABA"), &b"ABCBDAB"[..]);
